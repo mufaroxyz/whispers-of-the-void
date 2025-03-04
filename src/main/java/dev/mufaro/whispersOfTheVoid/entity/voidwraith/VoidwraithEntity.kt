0f3752366f8bbf30074fb3ed8d77d032b564142c
mufaroxyz <mufaro.priv@gmail.com>
2025-03-04 12:33:43 +0100

    feat: set `isAttackable` and `isCollidable` to false for VoidwraithEntity

--- src/main/java/dev/mufaro/whispersOfTheVoid/entity/voidwraith/VoidwraithEntity.kt
+++ src/main/java/dev/mufaro/whispersOfTheVoid/entity/voidwraith/VoidwraithEntity.kt
@@ -1,6 +1,5 @@
 package dev.mufaro.whispersOfTheVoid.entity.voidwraith
 
-import dev.mufaro.whispersOfTheVoid.WhispersOfTheVoid
 import net.minecraft.command.argument.EntityAnchorArgumentType
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.attribute.DefaultAttributeContainer
@@ -9,8 +8,6 @@ import net.minecraft.entity.mob.MobEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
-import kotlin.math.atan2
-import kotlin.math.sqrt
 
 class VoidwraithEntity(entityType: EntityType<out VoidwraithEntity>, world: World) : MobEntity(entityType, world) {
     companion object {
@@ -24,7 +21,15 @@ class VoidwraithEntity(entityType: EntityType<out VoidwraithEntity>, world: Worl
         return true
     }
 
-    fun getCloserPlayer(): PlayerEntity? {
+    override fun isCollidable(): Boolean {
+        return false
+    }
+
+    override fun isAttackable(): Boolean {
+        return false
+    }
+
+    private fun getClosestPlayer(): PlayerEntity? {
         val player = world.getClosestPlayer(this, 80.0)
         return player
     }
@@ -32,23 +37,10 @@ class VoidwraithEntity(entityType: EntityType<out VoidwraithEntity>, world: Worl
     override fun tick() {
         super.tick()
 
-        val player = getCloserPlayer()
+        val player = getClosestPlayer()
         val playerEyePos: Vec3d = player?.eyePos ?: Vec3d.ZERO
 
         if (player != null) {
-//            val deltaX = player.x - this.x
-//            val deltaZ = player.z - this.z
-//            val newYaw = (atan2(deltaZ, deltaX) * 180.0 / Math.PI).toFloat() - 90.0f
-//
-//            val deltaY = (player.eyeY - this.eyeY)
-//            val horizontalDistance = sqrt(deltaX * deltaX + deltaZ * deltaZ)
-//            val newPitch = -(atan2(deltaY, horizontalDistance) * 180.0 / Math.PI).toFloat()
-//
-//            this.bodyYaw = newYaw
-//            this.headYaw = newYaw
-//            this.pitch = newPitch
-//
-//            this.setYaw(newYaw)
             this.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, playerEyePos)
         }
 
